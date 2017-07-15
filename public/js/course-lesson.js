6d313ced78fcd08d29202c1306ae6b2e66399fbc
code1031 <wangzijie1031@qq.com>
2017-07-15 15:38:58 +0800

    实现课时展示功能

--- public/js/course-lesson.js
+++ public/js/course-lesson.js
@@ -1,5 +1,17 @@
 define(['jquery','template','util'],function($,template,util){
   // 设置菜单选中
   util.setMenu('/course/add');
-  
+  // 获取课程ID
+  var csId = util.qs('cs_id',location.search);
+  // 根据ID查询课时信息
+  $.ajax({
+    type : 'get',
+    url : '/api/course/lesson',
+    data : {cs_id : csId},
+    dataType : 'json',
+    success : function(data){
+      var html = template('lessonTpl',data.result);
+      $('#lessonInfo').html(html);
+    }
+  });
 });
